ed42f225c60d0a6048a1aac1c332a4629dec91ad
DESKTOP-8IAJS5U\User <glau_76@mail.ru>
2022-09-30 23:28:31 +0300

    Разбил по папкам. Тесты с Pytest + fixtures

---
+++ scripts/second_pytest/test_lesson3_2_step13.py
@@ -0,0 +1,56 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+
+class TestAbs(unittest.TestCase):
+    def test_001(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+        input3.send_keys("test@mail.tst")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+        browser.quit()
+
+    def test_002(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+        input3.send_keys("test@mail.tst")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+        browser.quit()
+
+
+if __name__ == "__main__":
+    pytest.main()
